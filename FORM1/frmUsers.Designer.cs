c09cd3cf5978211e62987d2b12037eb5aa76c3a1
rylerodil <rjdrodil@pcu.edu.ph>
2026-05-15 23:03:23 +0800

    update in frmuser

--- FORM1/frmUsers.Designer.cs
+++ FORM1/frmUsers.Designer.cs
@@ -28,12 +28,272 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbFname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbMname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbEmailAdd = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbHomeAdd = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpBirthDate = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDeactivate = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbLname = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First name:";
+            // 
+            // tbFname
+            // 
+            this.tbFname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFname.Location = new System.Drawing.Point(17, 37);
+            this.tbFname.Name = "tbFname";
+            this.tbFname.Size = new System.Drawing.Size(275, 30);
+            this.tbFname.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Middle name:";
+            // 
+            // tbMname
+            // 
+            this.tbMname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbMname.Location = new System.Drawing.Point(17, 108);
+            this.tbMname.Name = "tbMname";
+            this.tbMname.Size = new System.Drawing.Size(275, 30);
+            this.tbMname.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Email Address:";
+            // 
+            // tbEmailAdd
+            // 
+            this.tbEmailAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEmailAdd.Location = new System.Drawing.Point(17, 242);
+            this.tbEmailAdd.Name = "tbEmailAdd";
+            this.tbEmailAdd.Size = new System.Drawing.Size(275, 30);
+            this.tbEmailAdd.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 285);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Home Address:";
+            // 
+            // tbHomeAdd
+            // 
+            this.tbHomeAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbHomeAdd.Location = new System.Drawing.Point(17, 313);
+            this.tbHomeAdd.Name = "tbHomeAdd";
+            this.tbHomeAdd.Size = new System.Drawing.Size(275, 30);
+            this.tbHomeAdd.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 359);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Birth Date:";
+            // 
+            // dtpBirthDate
+            // 
+            this.dtpBirthDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpBirthDate.FormattingEnabled = true;
+            this.dtpBirthDate.Location = new System.Drawing.Point(17, 387);
+            this.dtpBirthDate.Name = "dtpBirthDate";
+            this.dtpBirthDate.Size = new System.Drawing.Size(275, 33);
+            this.dtpBirthDate.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 432);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 25);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Username:";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUsername.Location = new System.Drawing.Point(17, 460);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(275, 30);
+            this.tbUsername.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 503);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(104, 25);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Password:";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(17, 531);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(275, 30);
+            this.tbPassword.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(139, 567);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(167, 56);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(309, 9);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(575, 524);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // btnDeactivate
+            // 
+            this.btnDeactivate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeactivate.Location = new System.Drawing.Point(751, 546);
+            this.btnDeactivate.Name = "btnDeactivate";
+            this.btnDeactivate.Size = new System.Drawing.Size(133, 46);
+            this.btnDeactivate.TabIndex = 5;
+            this.btnDeactivate.Text = "Deactivate";
+            this.btnDeactivate.UseVisualStyleBackColor = true;
+            this.btnDeactivate.Click += new System.EventHandler(this.btnDeactivate_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(596, 546);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(133, 46);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 141);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(109, 25);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Last name:";
+            // 
+            // tbLname
+            // 
+            this.tbLname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLname.Location = new System.Drawing.Point(17, 169);
+            this.tbLname.Name = "tbLname";
+            this.tbLname.Size = new System.Drawing.Size(275, 30);
+            this.tbLname.TabIndex = 1;
+            // 
+            // frmUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(897, 627);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDeactivate);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dtpBirthDate);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbHomeAdd);
+            this.Controls.Add(this.tbLname);
+            this.Controls.Add(this.tbMname);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbEmailAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbFname);
+            this.Controls.Add(this.label1);
+            this.Name = "frmUsers";
             this.Text = "frmUsers";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbFname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbMname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbEmailAdd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbHomeAdd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox dtpBirthDate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDeactivate;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbLname;
     }
 }
--- FORM1/frmUsers.cs
+++ FORM1/frmUsers.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,105 @@ namespace FORM1
         public frmUsers()
         {
             InitializeComponent();
+        }
+
+        MyDatabase db = new MyDatabase();
+        bool isUpdate = false;
+
+        private void frmUsers_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            frmHome frm = new frmHome();
+            frm.Show();
+        }
+
+        private void frmUsers_Load(object sender, EventArgs e)
+        {
+            isUpdate = false;
+            string query = "SELECT tbluserinformation.userID, tbllogincredentials.LoginID, tbluserinformation.firstname, " +
+                "tbluserinformation.middlename, tbluserinformation.lastname, tbluserinformation.emailAddress," +
+                " tbluserinformation.homeAddress, tbluserinformation.birthDate, tbllogincredentials.user_username as 'Username'," +
+                " tbllogincredentials.user_password as 'Password' FROM tbllogincredentials INNER JOIN tbluserinformation" +
+                " ON tbllogincredentials.userID = tbluserinformation.userID;";
+
+            dgvUsers.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgvUsers.DataSource = db.ExecuteReturnQuery(query);
+            dgvUsers.Columns[0].Visible = false;
+            dgvUsers.Columns[1].Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isUpdate == false)
+            {
+                string query = "INSERT INTO tbluserinformation (firstname, middlename, lastname, emailAddress, homeAddress, birthDate)" +
+                " VALUES (@fname, @mname, @lname, @email, @hadd, @bDate);" +
+                "SET @newUserID = LAST_INSERT_ID();" +
+                "INSERT INTO tbllogincredentials (userID, user_username, user_password) VALUES (@newUserID, @username, @password);";
+
+                int affectedRowCount = db.ExecuteNoReturnQuery(query,
+                    new MySqlParameter("@fname", tbFname.Text),
+                    new MySqlParameter("@mname", tbMname.Text),
+                    new MySqlParameter("@lname", tbLname.Text),
+                    new MySqlParameter("@email", tbEmailAdd.Text),
+                    new MySqlParameter("@hadd", tbHomeAdd.Text),
+                    new MySqlParameter("@bDate", dtpBirthDate.Value),
+                    new MySqlParameter("@username", tbUsername.Text),
+                    new MySqlParameter("@password", tbPassword.Text)
+                    );
+
+                if (affectedRowCount > 0)
+                {
+                    MessageBox.Show("Data Inserted!");
+                    frmUsers_Load(null, null);
+                }
+            }
+            else if (isUpdate == true)
+            {
+                //update process
+
+
+                isUpdate = false;
+            }
+        }
+
+        private void btnDeactivate_Click(object sender, EventArgs e)
+        {
+            if (dgvUsers.SelectedRows.Count > 0)
+            {
+
+                DialogResult result = MessageBox.Show("Are you sure you want to deactivate this account?", "Account Deactivation", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+
+                    int id = Convert.ToInt32(dgvUsers.SelectedRows[0].Cells[1].Value);
+                    string query = "UPDATE tbllogincredentials SET is_active = 0 where LoginID = @id";
+
+                    int affectedRows = db.ExecuteNoReturnQuery(query,
+                        new MySqlParameter("@id", id));
+                    if (affectedRows > 0)
+                    {
+                        MessageBox.Show("Account is deactivated!");
+                    }
+
+                }
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dgvUsers.SelectedRows.Count > 0)
+            {
+                DialogResult result = MessageBox.Show("Are you sure you want to update this account?", "Update Account", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    isUpdate = true;
+                    int idUserInfo = Convert.ToInt32(dgvUsers.SelectedRows[0].Cells[0].Value);
+                    int idLoginCredentials = Convert.ToInt32(dgvUsers.SelectedRows[0].Cells[1].Value);
+                    tbFname.Text = dgvUsers.SelectedRows[0].Cells[2].Value.ToString();
+
+                  
+                }
+            }
         }
     }
 }
